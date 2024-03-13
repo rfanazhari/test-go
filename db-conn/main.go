853c0617537d27a036b27c82c5a6a6339b5f3607
arfan <afz60.30@gmail.com>
2024-03-13 15:59:47 +0700

    add file

---
+++ db-conn/main.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"sync"
+)
+
+type Item struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type ItemStore struct {
+	sync.Mutex
+	items      map[int]Item
+	currentID  int
+}
+
+func NewItemStore() *ItemStore {
+	return &ItemStore{
+		items:     make(map[int]Item),
+		currentID: 1,
+	}
+}
+
+func (store *ItemStore) getItemsHandler(w http.ResponseWriter, r *http.Request) {
+	store.Lock()
+	defer store.Unlock()
+
+	items := make([]Item, 0, len(store.items))
+	for _, item := range store.items {
+		items = append(items, item)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(items)
+}
+
+func (store *ItemStore) postItemHandler(w http.ResponseWriter, r *http.Request) {
+	store.Lock()
+	defer store.Unlock()
+
+	var newItem Item
+	err := json.NewDecoder(r.Body).Decode(&newItem)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error decoding request body: %v", err), http.StatusBadRequest)
+		return
+	}
+
+	newItem.ID = store.currentID
+	store.currentID++
+	store.items[newItem.ID] = newItem
+
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(newItem)
+}
+
+func main() {
+	itemStore := NewItemStore()
+
+	http.HandleFunc("/items", func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			itemStore.getItemsHandler(w, r)
+		case http.MethodPost:
+			itemStore.postItemHandler(w, r)
+		default:
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		}
+	})
+
+	port := 8080
+	fmt.Printf("Starting server on :%d...\n", port)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+	}
+}
---
+++ read-json/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/bxcodec/faker/v3"
+	"io/ioutil"
+	"log"
+	"os"
+)
+
+type StructJson struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+func main() {
+	// Read the JSON file
+	fileName := "file.json"
+	content, err := ioutil.ReadFile(fileName)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	var data StructJson
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	log.Println("READ DATA OF FILE")
+	log.Println(fmt.Sprintf("Name : %s", data.Name))
+	log.Println(fmt.Sprintf("Email : %s", data.Email))
+	log.Println(fmt.Sprintf("Age : %d", data.Age))
+
+	newData := StructJson{
+		Name:  faker.Name(),
+		Age:   len(faker.Name()),
+		Email: faker.Name(),
+	}
+
+	log.Println("REWRITE DATA OF FILE")
+	log.Println(fmt.Sprintf("Name from %s to %s", data.Name, newData.Name))
+	log.Println(fmt.Sprintf("Email from %s to %s", data.Email, newData.Email))
+	log.Println(fmt.Sprintf("Age from %d to %d", data.Age, newData.Age))
+
+	log.Println("MODIFIED FILE JSON")
+
+	modified, err := json.Marshal(newData)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	err = ioutil.WriteFile(fileName, modified, os.ModePerm)
+	if err != nil {
+		fmt.Println("Failed for writing JSON file:", err)
+	}
+
+	log.Println("SUCCESS MODIFIED JSON FILE")
+}
